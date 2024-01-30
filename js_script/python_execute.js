4578ea3078092b6191e7207d11feed3e0f413693
Wiktor Ćwikła <115033827+Wiktor332@users.noreply.github.com>
2024-01-30 09:23:56 +0100

    upd

--- js_script/python_execute.js
+++ js_script/python_execute.js
@@ -1,15 +1,15 @@
-function runPythonScript() {
-    var pythonScriptPath = "./python/api.py";
-    fetch(`http://localhost:3000/run-python?script=${pythonScriptPath}`)
-      .then(response => response.text())
-      .then(result => console.log(result))
-      .catch(error => console.error(`Error executing Python script: ${error}`));
-  }
+// function runPythonScript() {
+//     var pythonScriptPath = "./python/api.py";
+//     fetch(`http://localhost:3000/run-python?script=${pythonScriptPath}`)
+//       .then(response => response.text())
+//       .then(result => console.log(result))
+//       .catch(error => console.error(`Error executing Python script: ${error}`));
+//   }
 
-  window.onload = function () {
-    runPythonScript();
-  };
+//   window.onload = function () {
+//     runPythonScript();
+//   };
 
-  setInterval(() => {
-    location.reload();
-  }, 5000);
+//   setInterval(() => {
+//     location.reload();
+//   }, 5000);
