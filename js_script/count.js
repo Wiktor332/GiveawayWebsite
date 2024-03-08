fa7103e3bb09b9322e0dbe9c2cc4ddd4f8982565
Wiktor332 <wiktor.stanislaw.cwikla@gmail.com>
2024-03-08 11:08:16 +0100

    wrapper_center

--- js_script/count.js
+++ js_script/count.js
@@ -3,12 +3,15 @@
 // const wrapper = document.querySelectorAll('.wrapper');
 // console.log('Number of objects inside cards:', numberOfObjects);
 
-window.addEventListener('DOMContentLoaded', () => {
-    const cardsElement = document.getElementById("cards");
-    numberOfObjects = cardsElement.childElementCount;
-    if (numberOfObjects > 3) {
-        wrapper.style.display = 'none';
-    } else {
-        wrapper.style.display = 'flex';
-    }
-})
+// function getCount() {
+// const cardsElement = document.getElementById("cards");
+//     numberOfObjects = cardsElement.childElementCount;
+//     if (numberOfObjects > 30) {
+//         wrapper.style.display = 'none';
+//     } else {
+//         wrapper.style.display = 'block';
+//     }
+// }
+// window.addEventListener('load', () => {
+//     getCount;
+// })
--- js_script/test.js
+++ js_script/test.js
@@ -1,222 +1,4 @@
 
-// Cards Module
-const CardsModule = (() => {
-    // Variable to store the text content of the tracked element
-let trackedElementText = "";
-
-// Function to determine the path based on trackedElementText
-function handleTrackedTextContent() {
-  let fetchPath = ''; // Default path
-  switch (trackedElementText) {
-    case 'Playstation':
-      fetchPath = './python/data_json/data_playstation.json';
-      break;
-    case 'Playstation 4':
-      fetchPath = './python/data_json/data_ps4.json';
-      break;
-    case 'Playstation 5':
-      fetchPath = './python/data_json/data_ps5.json';
-      break;
-    case 'Xbox':
-      fetchPath = './python/data_json/data_xbox.json';
-      break;
-    case 'Xbox One': 
-      fetchPath = './python/data_json/data_one.json';
-      break;
-    case 'Xbox Series XS':
-      fetchPath = './python/data_json/data_xs.json';
-      break;
-    case 'PC':
-      fetchPath = './python/data_json/data_pc.json';
-      break;
-    case 'ALL PC':
-      fetchPath = './python/data_json/data_pc.json';
-      break;
-    case 'Steam':
-      fetchPath = './python/data_json/data_steam.json';
-      break;
-    case 'Epic Games Store':
-      fetchPath = './python/data_json/data_epic.json';
-      break;
-    case 'GOG':
-      fetchPath = './python/data_json/data_gog.json';
-      break;
-    default:
-      fetchPath = './python/data_json/data_pc.json'
-      break;
-  }
-  return fetchPath;
-}
-
-// Function to fetch data based on the determined path
-function fetchData(path) {
-  fetch(path)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-    })
-    .then((completedata) => {
-      let data1 = "";
-      let count = 0;
-      completedata.forEach((values, index) => {
-        const checkboxID = `btnControl_${index}`;
-        data1 += `<div class="card">
-        <input type="checkbox" id="${checkboxID}"/>
-        <label class="btn" for="${checkboxID}">
-          <h1 class="title">${values.title}</h1>
-          <img src="${values.image}" alt="img" class="images">
-          <p class="description">${values.description}</p>
-          <p class="type">Type: ${values.type}</p>
-          <p class="instructions">${values.instructions}</p>
-          <a class="link" href="${values.gamerpower_url}" target="_blank">Click to receive</a>
-          <p class="info">Click for detailed instructions</p>
-        </label>
-      </div>`;
-      count++;
-      });
-      console.log("test objects:",count);
-      document.getElementById("cards").innerHTML = data1;
-    })
-
-    .catch((err) => {
-      console.error('Error fetching data:', err);
-    });
-}
-
-function updateTitleHeader(newText) {
-    if (window.innerWidth < 1300) {
-        trackedElementText = newText; // Update trackedElementText first
-        const titleHeader = document.querySelector('.cards-title');
-        if (titleHeader) {
-          const newTitle = `${newText} Games and DLC available to download:`;
-          titleHeader.textContent = newTitle;
-        }
-     }
-}
-
-function getCount() {
-  const cardsElement = document.getElementById("cards");
-  const cardElements = cardsElement.querySelectorAll(".card");
-  const count = cardElements.length;
-  console.log('Count:', count); // Log the count
-  return count;
-}
-
-
-// Function to load data based on URL parameter or default to PC data
-function loadDataFromURL() {
-  const urlParams = new URLSearchParams(window.location.search);
-  const selectedOption = urlParams.get('page');
-
-  if (selectedOption) {
-    const options = document.querySelectorAll('.menu li');
-    options.forEach(option => {
-      if (option.getAttribute('href') === selectedOption) {
-        trackedElementText = option.textContent;
-        const path = handleTrackedTextContent();
-        fetchData(path);
-        updateCardsHeader();
-        // Update title and selected text
-        selected.innerText = trackedElementText;
-        // Update UI with selected option
-        const selected = document.querySelector('.selected');
-        selected.innerText = trackedElementText;
-      }
-    });
-  } else {
-    trackedElementText = "PC"; // Set default to PC
-    const path = handleTrackedTextContent();
-    fetchData(path);
-    // Update UI with default option
-    const selected = document.querySelector('.selected');
-    selected.innerText = trackedElementText;
-  }
-}
-
-// Load data on page load
-window.addEventListener('load', () => {
-  loadDataFromURL();
-  getCount();
-});
-
-// Iterate through dropdown elements
-const dds = document.querySelectorAll('.dropdown');
-
-dds.forEach(dd => {
-  const select = dd.querySelector('.select');
-  const options = dd.querySelectorAll('.menu li'); // Changed to select anchor tags
-
-  const selected = dd.querySelector('.selected');
-
-  select.addEventListener('click', () => {
-    select.classList.toggle('select-clicked');
-  });
-
-  options.forEach(option => {
-    option.addEventListener('click', (event) => {
-      event.preventDefault();
-
-      const clickedText = option.textContent;
-      updateTitleHeader(clickedText);
-
-      trackedElementText = option.textContent;
-      selected.innerText = trackedElementText;
-  
-      document.getElementById("cards").innerHTML = '';
-
-      const path = handleTrackedTextContent();
-      fetchData(path);
-  
-      console.log('Clicked option:', trackedElementText); // Log the clicked option
-      console.log('Fetching data from path:', path); // Log the fetch path
-      
-  
-      // Change the URL based on the clicked option
-      const page = option.getAttribute('href'); // Get the href attribute of the clicked option
-      if (page) {
-        const url = new URL(window.location.href);
-        url.searchParams.set('page', page); // Set a URL parameter 'page' with the clicked option's href
-        window.history.pushState({}, '', url); // Change the URL without reloading the page
-        console.log('Changed URL:', window.location.href); // Log the changed URL
-      }
-  
-      select.classList.remove('select-clicked');
-
-      function loadDataFromURL() {
-        const urlParams = new URLSearchParams(window.location.search);
-        const selectedOption = urlParams.get('page');
-      
-        // If there's a selectedOption, load data accordingly
-        if (selectedOption) {
-          const options = document.querySelectorAll('.menu li a');
-          options.forEach(option => {
-            if (option.getAttribute('href') === selectedOption) {
-              trackedElementText = option.textContent;
-              const path = handleTrackedTextContent();
-              fetchData(path);
-              
-              // Update UI with selected option
-              const selected = document.querySelector('.selected');
-              selected.innerText = trackedElementText;
-            }
-          });
-        }
-      }
-      
-      // Load data on page load
-      window.addEventListener('load', () => {
-        loadDataFromURL();
-      });
-    });
-  });
-});
-return {
-  loadDataFromURL, 
-};
-})();
-
 // Carousel Module 
 const CarouselModule = (() => {
   // Variable to store the text content of the tracked element
@@ -269,6 +51,11 @@ return fetchPath_wrapper;
 }
 
 
+// fetchData(path)
+//   .then(({count}) => {
+//     console.log('Count:', count);
+//   });
+
 
 function fetchData_wrapper(path_wrapper) {
 fetch(path_wrapper)
@@ -285,7 +72,6 @@ fetch(path_wrapper)
 
     let data_wrapper = "";
     topThree.forEach((values, index) => {
-      const topThree = completedata_wrapper.slice(0,3);
 
       let data_wrapper = "";
       topThree.forEach((values, index) => {
@@ -331,8 +117,6 @@ function displayWrapper_width() {
   }
 } 
 
-
-
 // Initial fetch when the page loads
 fetchData_wrapper('your_path_here');
 // Function to load data based on URL parameter or default to PC data
@@ -362,14 +146,6 @@ function loadDataFromURL() {
     const selected = document.querySelector('.selected');
     selected.innerText = trackedElementText;
   }
-}
-
-
-function getCount() {
-    const cardsElement = document.getElementById("cards");
-    const cardElements = cardsElement.querySelectorAll(".card");
-    console.log('Count:', count);
-    return cardElements.length;
 }
 
 // Load data on page load
@@ -467,5 +243,216 @@ return {
 };
 })();
 
+
+// Cards Module
+const CardsModule = (() => {
+  // Variable to store the text content of the tracked element
+let trackedElementText = "";
+
+// Function to determine the path based on trackedElementText
+function handleTrackedTextContent() {
+let fetchPath = ''; // Default path
+switch (trackedElementText) {
+  case 'Playstation':
+    fetchPath = './python/data_json/data_playstation.json';
+    break;
+  case 'Playstation 4':
+    fetchPath = './python/data_json/data_ps4.json';
+    break;
+  case 'Playstation 5':
+    fetchPath = './python/data_json/data_ps5.json';
+    break;
+  case 'Xbox':
+    fetchPath = './python/data_json/data_xbox.json';
+    break;
+  case 'Xbox One': 
+    fetchPath = './python/data_json/data_one.json';
+    break;
+  case 'Xbox Series XS':
+    fetchPath = './python/data_json/data_xs.json';
+    break;
+  case 'PC':
+    fetchPath = './python/data_json/data_pc.json';
+    break;
+  case 'ALL PC':
+    fetchPath = './python/data_json/data_pc.json';
+    break;
+  case 'Steam':
+    fetchPath = './python/data_json/data_steam.json';
+    break;
+  case 'Epic Games Store':
+    fetchPath = './python/data_json/data_epic.json';
+    break;
+  case 'GOG':
+    fetchPath = './python/data_json/data_gog.json';
+    break;
+  default:
+    fetchPath = './python/data_json/data_pc.json'
+    break;
+}
+return fetchPath;
+}
+
+// Function to fetch data based on the determined path
+function fetchData(path) {
+fetch(path)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then((completedata) => {
+    let data1 = "";
+    let count = 0;
+    completedata.forEach((values, index) => {
+      const checkboxID = `btnControl_${index}`;
+      data1 += `<div class="card">
+      <input type="checkbox" id="${checkboxID}"/>
+      <label class="btn" for="${checkboxID}">
+        <h1 class="title">${values.title}</h1>
+        <img src="${values.image}" alt="img" class="images">
+        <p class="description">${values.description}</p>
+        <p class="type">Type: ${values.type}</p>
+        <p class="instructions">${values.instructions}</p>
+        <a class="link" href="${values.gamerpower_url}" target="_blank">Click to receive</a>
+        <p class="info">Click for detailed instructions</p>
+      </label>
+    </div>`;
+    count++;
+    });
+    console.log("test objects:",count);
+    document.getElementById("cards").innerHTML = data1;
+  })
+
+  .catch((err) => {
+    console.error('Error fetching data:', err);
+    return {data:null, count: null};
+    
+  });
+  
+}
+
+function updateTitleHeader(newText) {
+  if (window.innerWidth < 1300) {
+      trackedElementText = newText; // Update trackedElementText first
+      const titleHeader = document.querySelector('.cards-title');
+      if (titleHeader) {
+        const newTitle = `${newText} Games and DLC available to download:`;
+        titleHeader.textContent = newTitle;
+      }
+   }
+}
+
+// Function to load data based on URL parameter or default to PC data
+function loadDataFromURL() {
+const urlParams = new URLSearchParams(window.location.search);
+const selectedOption = urlParams.get('page');
+
+if (selectedOption) {
+  const options = document.querySelectorAll('.menu li');
+  options.forEach(option => {
+    if (option.getAttribute('href') === selectedOption) {
+      trackedElementText = option.textContent;
+      const path = handleTrackedTextContent();
+      fetchData(path);
+      updateCardsHeader(newText);
+      // Update title and selected text
+      selected.innerText = trackedElementText;
+      // Update UI with selected option
+      const selected = document.querySelector('.selected');
+      selected.innerText = trackedElementText;
+    }
+  });
+} else {
+  trackedElementText = "PC"; // Set default to PC
+  const path = handleTrackedTextContent();
+  fetchData(path);
+  // Update UI with default option
+  const selected = document.querySelector('.selected');
+  selected.innerText = trackedElementText;
+}
+}
+
+// Load data on page load
+window.addEventListener('load', () => {
+loadDataFromURL();
+});
+
+// function getCount() {
+//   const cards
+// }
+
+// Iterate through dropdown elements
+const dds = document.querySelectorAll('.dropdown');
+
+dds.forEach(dd => {
+const select = dd.querySelector('.select');
+const options = dd.querySelectorAll('.menu li'); // Changed to select anchor tags
+
+const selected = dd.querySelector('.selected');
+
+select.addEventListener('click', () => {
+  select.classList.toggle('select-clicked');
+});
+
+options.forEach(option => {
+  option.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const clickedText = option.textContent;
+    updateTitleHeader(clickedText);
+
+    trackedElementText = option.textContent;
+    selected.innerText = trackedElementText;
+
+    document.getElementById("cards").innerHTML = '';
+
+    const path = handleTrackedTextContent();
+    fetchData(path);      
+
+    // Change the URL based on the clicked option
+    const page = option.getAttribute('href'); // Get the href attribute of the clicked option
+    if (page) {
+      const url = new URL(window.location.href);
+      url.searchParams.set('page', page); // Set a URL parameter 'page' with the clicked option's href
+      window.history.pushState({}, '', url); // Change the URL without reloading the page
+      console.log('Changed URL:', window.location.href); // Log the changed URL
+    }
+
+    select.classList.remove('select-clicked');
+
+    function loadDataFromURL() {
+      const urlParams = new URLSearchParams(window.location.search);
+      const selectedOption = urlParams.get('page');
+    
+      // If there's a selectedOption, load data accordingly
+      if (selectedOption) {
+        const options = document.querySelectorAll('.menu li a');
+        options.forEach(option => {
+          if (option.getAttribute('href') === selectedOption) {
+            trackedElementText = option.textContent;
+            const path = handleTrackedTextContent();
+            fetchData(path);
+            
+            // Update UI with selected option
+            const selected = document.querySelector('.selected');
+            selected.innerText = trackedElementText;
+          }
+        });
+      }
+    }
+    
+    // Load data on page load
+    window.addEventListener('load', () => {
+      loadDataFromURL();
+    });
+  });
+});
+});
+return {
+loadDataFromURL, 
+};
+})();
 
 
